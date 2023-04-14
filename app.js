add000fe6116c6228854af56cc59d1ff69ed4660
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-14 17:35:12 -0400

    added post method to app.js and updated form attributes in signup.html.

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
-    res.sendFile(__dirname + "/signup.html");
-    // res.send("Successully loaded!")
+    res.sendFile(__dirname + "/signup.html");    
+})
+
+app.post("/", function(req, res) {
+
+    var firstName = req.body.fName;
+    var lastname = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName, lastname, email);
 })
 
 app.listen(3000, function() {
