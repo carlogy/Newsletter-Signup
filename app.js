e2c811bdf1788de9eafab50462e52886bbe28081
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-04-15 11:48:19 -0400

    completed the failure and succes paths.

--- app.js
+++ app.js
@@ -14,13 +14,68 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-    var firstName = req.body.fName;
-    var lastname = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastname = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed", 
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastname,
+                }
+            }
+        ]
+
+    };
+
+    
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/cf80559ed1"
+
+  
+
+    const options = {
+        method: "POST",
+        auth: "carlo1:e651dcec181ad92c993efdcc32e8841e-us21"
+    }
+
+   const request = https.request(url, options, function(response) {
+        
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+        
+    })
+
+    request.write(jsonData);
+    request.end();
+
+
+
 
     console.log(firstName, lastname, email);
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
 })
 
 app.listen(3000, function() {
     console.log("Server running on port: 3000.");
 })
+
+// apiKey: e651dcec181ad92c993efdcc32e8841e-us21
+
+// ListID: cf80559ed1
